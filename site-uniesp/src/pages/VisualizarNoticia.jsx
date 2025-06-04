a4bc24563fc8129d2d3557f493d233f9869d952e
JVamarante <joaovamarante@gmail.com>
2025-06-04 20:05:05 -0300

    commitVisuNoticia

--- site-uniesp/src/pages/VisualizarNoticia.jsx
+++ site-uniesp/src/pages/VisualizarNoticia.jsx
@@ -1,8 +1,37 @@
-import React from 'react'
+import React, { useReducer } from 'react'
+import {useEffect } from 'react'
+import { useParams } from 'react-router-dom';
+import axios from 'axios';
+import { Card } from 'react-bootstrap';
+
+
 
 const VisualizarNoticia = () => {
+    const { id } = useParams();
+    const [noticia, setNoticia] = useState({});
+    const urlBase = "http://localhost:3000";
+
+    useEffect(() => {
+      axios.get(`${urlBase}/noticias/${id}`)
+      .then(response => setNoticia(response.data))
+      .catch(error => {
+        console.error("Erro ao buscar os detalhes da notícia:", error);
+      });
+    }, [id]);
+  
+
+
   return (
-    <div>VisualizarNoticia</div>
+    <Container className="py-4">
+      <Card className="shadow-sm">
+        <Card.Body>
+          <Card.Title>{noticia.titulo}</Card.Title>
+          <Card.Text>{noticia.texto}</Card.Text>
+        </Card.Body>
+
+      </Card>
+
+    </Container>
   )
 }
 
